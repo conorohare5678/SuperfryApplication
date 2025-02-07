b41d9f49fa8bacd25e5942ba0535466d71ccb475
conorohare5678 <conorohare5678@hotmail.com>
2025-02-07 11:04:29 +0000

    Design Changes

--- superfry-application/src/Pages/Login.js
+++ superfry-application/src/Pages/Login.js
@@ -1,9 +1,28 @@
 import React from "react";
+import picture from "../images/Food.jpg"
 
 const Login = () =>{
 
     return(
         <>
+        <div className="image-container">
+                    <div className="overlay"></div>
+                        <img src={picture} alt="Fish and chips" className="homepage-background"/>
+                        <form className="login-overlay">
+                            <label>Enter your Username: </label>
+                            <br/>
+                            <input type="text"/>
+
+                            <br/>
+                            <label>Enter your Password: </label>
+                            <br/>
+                            <input type="password"/>
+                            <br/>
+                            <button>Login</button>
+
+                        </form>
+                        
+                </div>
         </>
     )
 }
