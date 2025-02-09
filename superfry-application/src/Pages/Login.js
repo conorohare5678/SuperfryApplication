f684ff6a95f2ead87afe9f1a340f4a601b6a6f38
40262408 <cohare49@qub.ac.uk>
2025-02-09 23:29:37 +0000

    login deign changes

--- superfry-application/src/Pages/Login.js
+++ superfry-application/src/Pages/Login.js
@@ -1,28 +1,35 @@
 import React from "react";
 import picture from "../images/Food.jpg"
+import picture2 from "../images/fish.PNG"
 
-const Login = () =>{
+const Login = () => {
 
-    return(
+    return (
         <>
-        <div className="image-container">
-                    <div className="overlay"></div>
-                        <img src={picture} alt="Fish and chips" className="homepage-background"/>
-                        <form className="login-overlay">
-                            <label>Enter your Username: </label>
-                            <br/>
-                            <input type="text"/>
+            <div className="image-container">
+                <div className="overlay"></div>
+                <img src={picture} alt="Fish and chips" className="homepage-background" />
 
-                            <br/>
-                            <label>Enter your Password: </label>
-                            <br/>
-                            <input type="password"/>
-                            <br/>
-                            <button>Login</button>
+                <form className="login-overlay">
+                    <img src={picture2}
+                        alt="fish"
+                    />
+                    <div className="container-overlay">
+                        <br />
+                        <label>Enter your Username: </label>
+                        <br />
+                        <input type="text" />
+                        <br />
+                        <label>Enter your Password: </label>
+                        <br />
+                        <input type="password" />
+                        <br />
+                        <button>Login</button>
+                    </div>
+                </form>
 
-                        </form>
-                        
-                </div>
+
+            </div>
         </>
     )
 }
