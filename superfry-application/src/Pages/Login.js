186f7fb36286ef978d3879cb44db964746a5b5f0
conorohare5678 <conorohare5678@hotmail.com>
2025-02-16 23:12:07 +0000

    Login function working

--- superfry-application/src/Pages/Login.js
+++ superfry-application/src/Pages/Login.js
@@ -1,23 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 import picture from "../images/Food.jpg"
 import picture2 from "../images/fish.PNG"
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
     const navigate = useNavigate();
 
-    const EmployeePage = () =>{
-        navigate('/EmployeePage')
-    }
+    // const EmployeePage = () =>{
+    //     navigate('/EmployeePage')
+    // }
 
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('http://localhost:5322/login-user',{
+                userName,
+                password
+            });
+
+            const data = response.data;
+
+              if (data.status === "ok") {
+                // Save the token in local storage or context
+              localStorage.setItem('token', data.data);
+              localStorage.setItem('userName', userName)
+              navigate('/EmployeePage');
+              } else {
+              setError(data.error);
+              }
+        } catch (error) {
+            setError('an error occurred.')
+            
+        }
+    }
     return (
         <>
             <div className="image-container">
                 <div className="overlay"></div>
                 <img src={picture} alt="Fish and chips" className="homepage-background" />
 
-                <form className="login-overlay">
+                <form className="login-overlay" onSubmit={handleLogin}>
                     <img src={picture2}
                         alt="fish"
                     />
@@ -25,13 +52,24 @@ const Login = () => {
                         <br />
                         <label>Enter your Username: </label>
                         <br />
-                        <input type="text" />
+                        <input 
+                        type="text" 
+                        value={userName}
+                        onChange={(e) => setUserName(e.target.value)}
+                        required
+                        />
                         <br />
                         <label>Enter your Password: </label>
                         <br />
-                        <input type="password" />
+                        <input
+                        type="password" 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        />
                         <br />
-                        <button onClick={EmployeePage}>Login</button>
+                        {error && <p style={{ color: 'red' }}>{error}</p>}
+                        <button type="submit" >Login</button>
                     </div>
                 </form>
 
--- superfry-application/src/Pages/employeePage.js
+++ superfry-application/src/Pages/employeePage.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import {useNavigate} from 'react-router-dom';
 
 const EmployeePage = () =>{
 
+    const navigate = useNavigate();
+    const token = window.localStorage.getItem('token');
+    const userName = window.localStorage.getItem('userName');
+
+    const logoutButton = () =>{
+        window.localStorage.removeItem('token');
+        window.localStorage.removeItem('userName')
+        navigate("/Login");
+    }
+
+    useEffect(() =>{
+        if(!token || !userName){
+            navigate("/Login")
+        }
+    })
+
     return(
         <>
+        <div className="logout-button">
+            <Button onClick={logoutButton}>Log out</Button>
+        </div>
     <div className="cardContainer">
        <Card style={{ width: '18rem' }}>
          <Card.Img variant="top" src="holder.js/100px180" />
